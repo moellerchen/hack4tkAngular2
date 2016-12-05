9c5a3f29df75fa930f691312d17ec51e994531ca
Möller, Catharina <catharina.moeller@mt-ag.com>
2016-12-05 23:24:31 +0100

    Overview component

---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'overview',
+    templateUrl: 'app/overview/overview.component.html'
+})
+export class OverviewComponent {
+
+}
